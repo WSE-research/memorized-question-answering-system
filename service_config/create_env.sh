0ce7bef09441c6e0d101de00b1f89765ae1cac51
Calvin <dev@doernchen.eu>
2023-07-05 19:33:21 +0200

    fake-question-answering-system 0.1.29
    bugfix in deployment config

--- service_config/create_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build Docker Image
-echo ${{ secrets.QADO_TRIPLESTORE_URL }}
-while read line; do
-
-    echo ${{ "secrets." echo ${line::-1} }}
-    echo $line
-done < service_config/files/template.env
