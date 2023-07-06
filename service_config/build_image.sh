59512af4b20e34e569099a9bc421314c2a119945
Calvin <dev@doernchen.eu>
2023-07-06 13:32:05 +0200

    fake-question-answering-system 0.1.32
    bugfix in deployment config

--- service_config/build_image.sh
+++ service_config/build_image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "$QADO_TRIPLESTORE_URL"
+
 # replace secrets
 if [ -z "$QADO_TRIPLESTORE_URL" ]
 then
   echo "QADO_TRIPLESTORE_URL is not set. Check your secrets."
-  exit
+  exit 1
 else
   sed -i "s/SECRETS_QADO_TRIPLESTORE_URL/$QADO_TRIPLESTORE_URL/g" ./service_config/files/.env
 fi
@@ -12,7 +14,7 @@ fi
 if [ -z "$QADO_TRIPLESTORE_DATABASE" ]
 then
   echo "QADO_TRIPLESTORE_DATABASE is not set. Check your secrets."
-  exit
+  exit 1
 else
   sed -i "s/SECRETS_QADO_TRIPLESTORE_DATABASE/$QADO_TRIPLESTORE_DATABASE/g" ./service_config/files/.env
 fi
@@ -20,7 +22,7 @@ fi
 if [ -z "$QADO_TRIPLESTORE_USERNAME" ]
 then
   echo "QADO_TRIPLESTORE_USERNAME is not set. Check your secrets."
-  exit
+  exit 1
 else
   sed -i "s/SECRETS_QADO_TRIPLESTORE_USERNAME/$QADO_TRIPLESTORE_USERNAME/g" ./service_config/files/.env
 fi
@@ -28,7 +30,7 @@ fi
 if [ -z "$QADO_TRIPLESTORE_PASSWORD" ]
 then
   echo "QADO_TRIPLESTORE_PASSWORD is not set. Check your secrets."
-  exit
+  exit 1
 else
   sed -i "s/SECRETS_QADO_TRIPLESTORE_PASSWORD/$QADO_TRIPLESTORE_PASSWORD/g" ./service_config/files/.env
 fi
